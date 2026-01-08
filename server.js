3259b9fb03330e9ce2b095a277a2bfd8fa09e607
rlife <rlife0896@gmail.com>
2026-01-08 18:27:20 +0530

    feat: add CORS support and update dependencies in package.json

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import dotenv from "dotenv";
+import cors from "cors";
 dotenv.config();
 
 import app from "./src/app.js";
@@ -7,6 +8,14 @@ import connectDB from "./src/config/db.js";
 const PORT = process.env.PORT || 5000;
 
 connectDB();
+
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "https://anpr.nexcorealliance.com"
+  ]
+}));
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
